57fe48b320faf09dc7c1a9f9bf342f5db5922349
gabriele.pellegrini <gabriele.pellegrini@AULA75-PC02.itib.local>
2026-04-01 12:48:32 +0200

    classe Mossa

---
+++ PrjPokemon/src/main/java/com/mycompany/prjpokemon/Mossa.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prjpokemon;
+
+/**
+ *
+ * @author gabriele.pellegrini
+ */
+public class Mossa {
+   private String mosse;
+   private int danno;
+
+    public Mossa(String mosse, int danno) {
+        this.mosse = mosse;
+        this.danno = danno;
+    }
+
+    public String getMosse() {
+        return mosse;
+    }
+
+    public int getDanno() {
+        return danno;
+    }
+   
+   
+}
