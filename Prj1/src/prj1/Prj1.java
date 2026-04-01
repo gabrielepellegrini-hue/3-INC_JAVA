7b21553893a58954178515752811e333cf945926
gabriele.pellegrini <gabriele.pellegrini@AULA75-PC02.itib.local>
2026-04-01 11:21:20 +0200

    Creazione di Prj1

---
+++ Prj1/src/prj1/Prj1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prj1;
+
+/**
+ *
+ * @author gabriele.pellegrini
+ */
+public class Prj1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
